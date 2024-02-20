a2042a9395f93ba538a0c9e3c17e5b4b699b6c82
Arpan Das <dasarpan471@gmail.com>
2024-02-20 19:26:53 +0530

    create a basic api in fast api

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+This module provides a FastAPI application with two endpoints.
+"""
 from typing import Union
 
 from fastapi import FastAPI
@@ -7,9 +10,25 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """
+    A simple function to handle requests to the root URL ("/").
+    
+    Returns:
+        dict: A dictionary containing a simple greeting.
+    """
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    """
+    A function to handle requests to "/items/{item_id}" URL.
+    
+    Args:
+        item_id (int): The ID of the item.
+        q (Union[str, None], optional): A query parameter (string or None). Defaults to None.
+        
+    Returns:
+        dict: A dictionary containing the item ID and the query parameter.
+    """
     return {"item_id": item_id, "q": q}
